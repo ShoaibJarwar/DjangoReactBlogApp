b7dcc14e7676274e9e848b273da11f55b07a03e0
ShoaibJarwar <mshoaibjarwar1256@gmail.com>
2025-09-11 19:10:53 +0500

    Blog App Backend

--- blogapi/blogapi/settings.py
+++ blogapi/blogapi/settings.py
@@ -99,7 +99,7 @@ SIMPLE_JWT = {
 
 MEDIA_URL ="/media/"
 # # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = BASE_DIR / "media" 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
